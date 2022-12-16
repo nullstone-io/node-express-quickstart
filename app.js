62a070bb14f8d3ade2889390ac7ccf1428620735
Brad Sickles <brad.sickles@gmail.com>
2022-12-16 09:58:36 -0500

    1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 9000
 
 app.get('/', (req, res) => {
     console.log('GET /')
-    res.send('Hello World!')
+    res.send('Hello World! (Test 1)')
 })
 
 app.listen(port, () => {
